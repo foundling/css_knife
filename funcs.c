07c76214decc2e92deafcb7d381eb658eea0d358
Alex Ramsdell <alexramsdell@gmail.com>
2015-01-20 19:03:09 -0500

    as usual, need to rename vars so i can think straight again.

--- funcs.c
+++ funcs.c
@@ -6,73 +6,87 @@
 char property_buffer[MAX_PROPERTY + 1];
 char key_buffer[MAX_KEY + 1];
 
-struct table
-{
-  struct style_block * next;
-};
-
-
-struct style_block
-{
-  char selector[MAX_KEY + 1];
-  struct property * property_list;
-};
-
-// linked list of properties
-struct property
-{
-  struct property * next;
-  char name[MAX_PROP_NAME];
-  char value[MAX_PROP_VALUE];
-};
 
 int parse_key_property(FILE * fp)
 {
   int c;
   int state = OUT;
   int pbc = 0, kbc = 0;
-  struct table Table = {0};
-  //struct style_block *style_block_ptr = malloc(sizeof(struct style_block));
+  struct rule_block Rule_Block;
+  struct rule_block * sp = &Rule_Block;
   
 	while ( (c=getc(fp)) != EOF)
   {
-		// create style_block on heap
-    struct style_block *style_block_ptr = malloc(sizeof(struct style_block));
-		// create property_list on heap
-		struct property * property_ptr = malloc(sizeof(struct property));
-		// style_block gets the address of property list just allocated
-		style_block_ptr->property_list = property_ptr; 
+		// pointer to a container for a style block. init to null
 
 		// accumulate key name while outside of code block
 		if ( (state == OUT) && (c != '{') )
 		{
 			key_buffer[kbc++] = c;
-		}
+    }
 		// switch to IN on opening brace
 		else if ( (state == OUT) && (c == '{') )
 		{
 			state = IN;
-		}
+    }
 		// store the property data in buf while not closing brace
-		else if ( (state == IN) && (c != '}') && (c != '\n') )
+		else if ( (state == IN) && (c != '}') )
 		{
 			property_buffer[pbc++] = c;
-		}
+    }
 		// exit process key property data on closing brace
 		else if ( (state == IN) && (c == '}') )
 		{
 			state = OUT;
-      //printf("%s\n",key_buffer);
-			// copy key_buffer into style_block_ptr's selector field
-      strcpy(style_block_ptr->selector,key_buffer);
-      strcpy(style_block_ptr->property_list->name,property_buffer);
-			printf("%s\n",style_block_ptr->selector);
-			printf("%s\n",style_block_ptr->property_list->name);
-
-      //printf("%s\n",style_block_ptr->selector);
+			strncpy(sp->key,key_buffer,MAX_KEY);
+      printf("%s\n",sp->key); 
       pbc = 0, kbc = 0;
       memset(key_buffer,0,sizeof(key_buffer));
       memset(property_buffer,0,sizeof(key_buffer));
+		}
+  }
+  return 0;
+}
+
+int parse_name_value(char * property_block, struct property_list * property_list_head_ptr) 
+{
+
+  int i;
+  int state = NAME;
+  int block_len = strlen(property_block);
+  char name[MAX_PROP_NAME + 1] = {0};
+  char value[MAX_PROP_VALUE + 1] = {0};
+  int nc, vc;
+	nc = vc = 0;
+
+  for (i=0;block_len;i++)
+  {
+    if ( (state == NAME) && (property_block[i] != ':'))
+    {
+      name[nc++] = property_block[i];     
+    }    
+    else if ( (state == NAME) && (property_block[i] == ':'))
+    {
+      state = VALUE;
+    }
+    else if ( (state == VALUE) && (property_block[i] != ';') )
+    {
+      value[vc++] = property_block[i];
+    }
+		else if ( (state == VALUE) && (property_block[i] == ';') )
+		{
+		  state = NAME;
+			// create new property node
+			// new node gets put first
+			// point new node's next to property list
+			// point property list ptr to NULL so it's at the end
+
+      // copy data to new propery_node's name and val fields
+
+			vc = nc = 0;
+      memset(name,0,sizeof(name));
+			memset(value,0,sizeof(value));
+
 		}
   }
   return 0;
--- header.h
+++ header.h
@@ -1,8 +1,31 @@
 #define   OUT             0 
 #define   IN              1 
+#define   NAME            0
+#define   VALUE           1
 #define   MAX_KEY         500
 #define   MAX_PROPERTY    1000
 #define   MAX_PROP_NAME   500
 #define   MAX_PROP_VALUE  1000
 
+// top level nodes are the css style code blocks
+// each style node has a key (aka html), a next pointert(to next style block), and an attached linked list of properties
+// each property node inthe property node list is a key and a value
+
+struct property_list
+{
+  struct property_list * next_property; // for linked list of properties
+  char name[MAX_PROP_NAME];
+  char value[MAX_PROP_VALUE];
+};
+
+struct rule_block
+{
+  char key[MAX_KEY + 1];
+  struct property_list * property_list_head; // for linked lists of property_lists
+  struct rule_block * next_rule_block; // for linked list of style blocks
+};
+
+
 int parse_key_property(FILE * fp);
+int parse_name_value(char * property_buffer, struct property_list * property_list_ptr);
+
