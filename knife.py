08c09c113cec25c35f744ffc79d6f2ca16cb848f
Alex Ramsdell <foundling@users.noreply.github.com>
2015-04-22 18:56:47 -0400

    Update knife.py

--- knife.py
+++ knife.py
@@ -1,8 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 
 # coding: utf-8
 
 ''' 
-  CSS Knife Sorts your CSS
+  CSS Knife
+  Usage: knife.py [-Dhi] file1 file2 file3 ... 
+  
+  -D    Exclude leading non-alphabetic characters from sort comparison. 
+  -h    Help.  Outputs this information.
+  -i    Modify the file in-place.  
 '''
 
 
@@ -10,8 +15,21 @@ import sys
 
 import tinycss
 
-def ignore_leading_dot(item):
-  if item['selector'].startswith('.'):
+  
+
+def usage():
+  usage_string = ''' 
+  Usage: knife.py [-Dhi] file1 file2 file3 ... 
+  
+  -D    Exclude leading non-alphabetic characters from sort comparison. 
+  -h    Help.  Outputs this information.
+  -i    Modify the file in-place.  
+  '''
+  print usage_string
+
+
+def ignore_leading_char(item,excluded_chars=".#"):
+  if item['selector'][0] in excluded_chars:
     return item['selector'][1:]
   else:
     return item['selector']
@@ -25,46 +43,51 @@ def style_errors(errors):
 
 def print_sorted_list(r_list):
   for rule in r_list:
-    print '%s {' % (rule['selector'])
+    print "{} {{".format(rule['selector'])
     for dec in rule['declarations']:
       for k,v in dec.iteritems():
-        print '    %s: %s;' % (k,v)
+        print "\t{}: {};".format(k,v)
     print '}\n'
 
-if len(sys.argv) < 2:
-  print 'no file argument'
-  sys.exit(1)
-else:
-  css_file = sys.argv[1]
+def main():
 
-try:
-  parser = tinycss.make_parser()
-  ss = parser.parse_stylesheet_file(css_file)
-except IOError:
-  print 'Could not open file %' % (css_file)
-  sys.exit(1)
+  if len(sys.argv) < 2:
+    usage()
+    sys.exit(1)
+  else:
+    css_file = sys.argv[1]
+
+  try:
+    parser = tinycss.make_parser()
+    ss = parser.parse_stylesheet_file(css_file)
+  except IOError:
+    print "Could not open file {}".format(css_file)
+    sys.exit(1)
 
 
-if ss.errors:
-  style_errors(ss.errors)
-  sys.exit(1)
+  if ss.errors:
+    style_errors(ss.errors)
+    sys.exit(1)
 
-app = {}
-app['rules'] = []
+  knife = {}
+  knife['rules'] = []
 
-for rule in ss.rules:
-  d = dict()
-  d['selector'] = rule.selector.as_css()
-  d['declarations'] = []
-  for dec in rule.declarations:
-    d['declarations'].append({dec.name: dec.value.as_css()})
-  app['rules'].append(d)
+  for rule in ss.rules:
+    d = dict()
+    d['selector'] = rule.selector.as_css()
+    d['declarations'] = []
+    for dec in rule.declarations:
+      d['declarations'].append({dec.name: dec.value.as_css()})
+    knife['rules'].append(d)
 
-# sort inner declarations
-for rule in app['rules']:
-  rule['declarations'].sort(key=lambda x: x.keys())
+  # sort inner declarations
+  for rule in knife['rules']:
+    rule['declarations'].sort(key=lambda x: x.keys())
 
-# sort selector blocks
-app['rules'].sort(key=ignore_leading_dot)
+  # sort selector blocks
+  knife['rules'].sort(key=ignore_leading_char)
 
-print_sorted_list(app['rules'])
+  print_sorted_list(knife['rules'])
+
+if __name__ == "__main__":
+  main()
