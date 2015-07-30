cfec7a7de8c5aec9745282b621d366610db3739d
Alex Ramsdell <foundling@users.noreply.github.com>
2015-07-30 12:09:52 -0400

    Update knife.py

--- knife.py
+++ knife.py
@@ -11,11 +11,7 @@ import tinycss
 
 def usage():
   usage_string = ''' 
-  Usage: knife.py [-Dhi] file1 file2 file3 ... 
-  
-  -D    Exclude leading non-alphabetic characters from sort comparison. 
-  -h    Help.  Outputs this information.
-  -i    Modify the file in-place.  
+  Usage: knife.py file.css 
   '''
   print usage_string
 
