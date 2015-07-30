41898f85bae24586ad05b189a1c759705de6a74d
Alex Ramsdell <foundling@users.noreply.github.com>
2015-07-30 12:06:38 -0400

    Update knife.py

--- knife.py
+++ knife.py
@@ -3,11 +3,7 @@
 
 ''' 
   CSS Knife
-  Usage: knife.py [-Dhi] file1 file2 file3 ... 
-  
-  -D    Exclude leading non-alphabetic characters from sort comparison. 
-  -h    Help.  Outputs this information.
-  -i    Modify the file in-place.  
+  Usage: knife.py file 
 '''
 
 import sys
